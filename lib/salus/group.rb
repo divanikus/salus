b43f065ecf9482bbcf3a641ef7dfb1061be3eb7e
divanikus <aperminov@gmail.com>
2017-11-05 19:12:20 +0300

    Thread safety?

--- lib/salus/group.rb
+++ lib/salus/group.rb
@@ -9,6 +9,8 @@ require "salus/metric/text"
 module Salus
   class Group
     extend Forwardable
+    include Lockable
+
     def_delegators :@metrics, :[], :key?, :values_at, :fetch, :length, :delete, :empty?
     attr_reader :title
 
@@ -34,65 +36,75 @@ module Salus
     end
 
     def group(title, &block)
-      unless @groups.key?(title)
-        @groups[title] = Group.new(&block)
-        if @cache.key?(title)
-          @groups[title].load(@cache[title])
-          @cache.delete(title)
+      synchronize do
+        unless @groups.key?(title)
+          @groups[title] = Group.new(&block)
+          if @cache.key?(title)
+            @groups[title].load(@cache[title])
+            @cache.delete(title)
+          end
         end
       end
     end
 
     def groups
-      @groups
+      synchronize { @groups }
     end
 
     def has_subgroups?
-      !@groups.empty?
+      synchronize { !@groups.empty? }
     end
 
     def value(title)
-      @metrics.key?(title) ? @metrics[title].value : nil
+      synchronize { @metrics.key?(title) ? @metrics[title].value : nil }
     end
 
     def keys(allow_mute=false)
-      if allow_mute
-        @metrics.keys
-      else
-        @metrics.keys.select { |x| !@metrics[x].mute? }
+      synchronize do
+        if allow_mute
+          @metrics.keys
+        else
+          @metrics.keys.select { |x| !@metrics[x].mute? }
+        end
       end
     end
 
     def values(allow_mute=false)
-      if allow_mute
-        @metrics.values
-      else
-        @metrics.values.select { |x| !x.mute? }
+      synchronize do
+        if allow_mute
+          @metrics.values
+        else
+          @metrics.values.select { |x| !x.mute? }
+        end
       end
     end
 
     def each(allow_mute=false, &block)
-      if allow_mute
-        @metrics.each(&block)
-      else
-        @metrics.select { |k, v| !v.mute? }.each(&block)
+      synchronize do
+        if allow_mute
+          @metrics.each(&block)
+        else
+          @metrics.select { |k, v| !v.mute? }.each(&block)
+        end
       end
     end
 
     def load(data)
       return if data.nil?
       return if data.empty?
-      if data.key?(:metrics)
-        types = Metric.descendants.map{ |x| x.name.split("::").last }
-        data[:metrics].each do |k, v|
-          next unless v.key?(:type)
-          next unless types.include?(v[:type])
-          @metrics[k] = Object.const_get("Salus::" + v[:type]).new
-          @metrics[k].load(v)
+      synchronize do
+        if data.key?(:metrics)
+          types = Metric.descendants.map{ |x| x.name.split("::").last }
+          data[:metrics].each do |k, v|
+            next unless v.key?(:type)
+            next unless types.include?(v[:type])
+            @metrics[k] = Object.const_get("Salus::" + v[:type]).new
+            @metrics[k].load(v)
+          end
         end
-      end
-      if data.key?(:groups)
-        @cache = data[:groups]
+        if data.key?(:groups)
+          @cache = data[:groups]
+        end
       end
     end
 
@@ -102,15 +114,17 @@ module Salus
 
     def to_h
       ret = {}
-      unless @metrics.empty?
-        ret[:metrics] = {}
-        @metrics.each { |k, v| ret[:metrics][k] = v.to_h }
+      synchronize do
+        unless @metrics.empty?
+          ret[:metrics] = {}
+          @metrics.each { |k, v| ret[:metrics][k] = v.to_h }
+        end
+        unless @groups.empty?
+          ret[:groups]  = {}
+          @groups.each  { |k, v| ret[:groups][k]  = v.to_h }
+        end
+        ret
       end
-      unless @groups.empty?
-        ret[:groups]  = {}
-        @groups.each  { |k, v| ret[:groups][k]  = v.to_h }
-      end
-      ret
     end
 
     def tick
